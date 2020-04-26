e96b51db4c3eef8bbc967acfbb191368baada6be
pietrodorto <s249615@studenti.polito.it>
2020-04-26 12:02:15 +0200

    controller implementato

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -2,11 +2,19 @@
  * Sample Skeleton for 'Scene.fxml' Controller Class
  */
 
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
 package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
@@ -16,8 +24,69 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="btnCercaIscrittiCorso"
+    private Button btnCercaIscrittiCorso; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMatricola"
+    private TextField txtMatricola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCompletamento"
+    private Button btnCompletamento; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtNome"
+    private TextField txtNome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCognome"
+    private TextField txtCognome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaCorsi"
+    private Button btnCercaCorsi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnIscrivi"
+    private Button btnIscrivi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtRisultato"
+    private TextArea txtRisultato; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+    @FXML
+    void doCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaIscrittiCorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCompletamentoAutomatico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert btnCercaIscrittiCorso != null : "fx:id=\"btnCercaIscrittiCorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCompletamento != null : "fx:id=\"btnCompletamento\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
